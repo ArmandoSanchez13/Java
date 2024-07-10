01dd15b017d70de5aa823c439907acac0dd2c828
ArmandoSanchez13 <148153490+ArmandoSanchez13@users.noreply.github.com>
2024-07-09 23:31:27 -0700

    Add files via upload

---
+++ DINING PHILOSOPHERS/DiningPhilosophers.java
@@ -0,0 +1,13 @@
+public class DiningPhilosophers
+{  
+   public static void main(String args[])
+   {
+      int n =5; //number of philosophers
+      DiningServer ds = new DiningServerImpl(n); //creates an instance and calls it ds
+ 
+      for(int i =0; i<n; i++){ //creates thread for philopsher class
+         //new Philosopher(i,ds)).run(); 
+       new Thread(new Philosopher(i,ds)).start(); 
+      }
+   }
+}
